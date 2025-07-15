3afb4af496d44dc7315e565c2b8440f59b69a842
Kassime <giovana.kassime.tds2023@gmail.com>
2025-07-15 11:32:40 -0300

    teste ssh

--- bolodemilho/flag1.sh
+++ bolodemilho/flag1.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-
-# Iniciar vários processos "isca" simples e idênticos em segundo plano.
-# O jogador terá que investigar cada um deles.
-
-(exec -a 'M4t3D0c3' sleep 3600) &
-
-
 # Inicia o servidor SSH em primeiro plano para manter o contêiner ativo.
 exec /usr/sbin/sshd -D
